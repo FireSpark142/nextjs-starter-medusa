69d1dee9dd8e5b8c3533e23831ffe975b975ddaf
Marcus <marcus.kehn@gmail.com>
2024-05-31 07:06:57 -0500

    Update medusa-config.js

--- .github/scripts/medusa-config.js
+++ .github/scripts/medusa-config.js
@@ -83,7 +83,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -94,7 +94,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
